2fcb28b1f19eaecab29ceafdac24d862d16991da
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-21 15:16:29 -0700

    Finish User controller methods

--- GoalApp/spec/controllers/users_controller_spec.rb
+++ GoalApp/spec/controllers/users_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe UsersController, type: :controller do
     context 'if user is not logged in' do
       it 'should render the new view' do
         get :new
-        expect(status).to be_success
+        expect(response).to be_success
         expect(response).to render_template('new')
       end
     end
@@ -15,15 +15,15 @@ RSpec.describe UsersController, type: :controller do
   describe '#create' do
     context 'params contain valid user' do
       it 'should redirect to user\'s show page' do
-        post :create, user: { email: "darlingtuna@savetheseas.org", password: "password" }
-        expect(response).to redirect_to(new_session_url)
+        post :create, params: { user: { email: "darlingtuna@savetheseas.org", password: "password" } }
+        expect(response).to redirect_to(user_url(User.find_by(email: controller.params[:user][:email])))
       end
     end
 
     context 'params are missing user information' do
       it 'should render new page' do
-        post :create, user: { email: "darlingtuna@savetheseas.org", password: "bogus" }
-        expect(response).to render_template('new')
+        post :create, params: { user: { email: "darlingtuna@savetheseas.org", password: "bogus" } }
+        expect(response).to render_template(:new)
         expect(flash[:errors]).to be_present
       end
     end
@@ -33,9 +33,9 @@ RSpec.describe UsersController, type: :controller do
   describe '#show' do
     it 'should render show view' do
       user = User.create(email: 'darlingtuna@savetheseas.org', password: 'password')
-      get :show, id: User.find(user)
+      get :show, params: { id: user.id }
       expect(response).to be_success
-      expect(response).to render_template('show')
+      expect(response).to render_template(:show)
     end
   end
 end
