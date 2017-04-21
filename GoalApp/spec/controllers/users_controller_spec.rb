db8790d0e37258ead13359cba8e9849b48921ab4
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-21 12:15:58 -0700

    Add specs for UsersController

--- GoalApp/spec/controllers/users_controller_spec.rb
+++ GoalApp/spec/controllers/users_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+lsrequire 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
@@ -10,27 +10,32 @@ RSpec.describe UsersController, type: :controller do
         expect(response).to render_template('new')
       end
     end
-
-    context 'if user is logged in' do
-      it 'should redirect to user show view'
-    end
   end
 
   describe '#create' do
     context 'params contain valid user' do
-      it 'should find valid user'
-      it 'should save the user'
-      it 'should redirect to show page'
+      it 'should redirect to user\'s show page' do
+        post :create, user: { email: "darlingtuna@savetheseas.org", password: "password" }
+        expect(response).to redirect_to(new_session_url)
+      end
     end
 
     context 'params are missing user information' do
-      it 'should not find valid user'
-      it 'should redirect to new view'
+      it 'should render new page' do
+        post :create, user: { email: "darlingtuna@savetheseas.org", password: "bogus" }
+        expect(response).to render_template('new')
+        expect(flash[:errors]).to be_present
+      end
     end
 
   end
 
   describe '#show' do
-    it 'should render show view'
+    it 'should render show view' do
+      user = User.create(email: 'darlingtuna@savetheseas.org', password: 'password')
+      get :show, id: User.find(user)
+      expect(response).to be_success
+      expect(response).to render_template('show')
+    end
   end
 end
